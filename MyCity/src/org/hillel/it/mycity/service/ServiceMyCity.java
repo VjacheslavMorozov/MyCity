b64d01c1a57df2afdebcc6bbe9363815ae8b26d7
ithillel-aminev <tim8917@gmail.com>
2014-04-10 16:24:06 +0300

    Add a comment. Offer to change ServiceMyCity interface. Also offer about implementation of the interface's methods.

--- MyCity/src/org/hillel/it/mycity/service/ServiceMyCity.java
+++ MyCity/src/org/hillel/it/mycity/service/ServiceMyCity.java
@@ -12,30 +12,59 @@ import org.hillel.it.mycity.model.entity.User;
 
 public interface ServiceMyCity {
 	//Establishment repository
+	// CREATE Establishment
+	/* @timur Тут можно попробовать реализовать шаблон FactoryMethod.
+	 * Тогда будет всего один метод типа
+	 * public void addEstablishment(EstType estType), 
+	 * где EstType - перечисление Restaurant, NightClub, Cinema.
+	 * 
+	 * Параметр Administrator administrator надо убирать и менять реализацию.
+	 * В системе будет существовать объект типа Person person, который будет содержать инфу
+	 * о залогинившемся пользователе. Реализация метода addEstablishmentRestaurant
+	 * будет обращаться к методу person.inGroup("admin"). 
+	 * 
+	 */ 
 	public void addEstablishmentRestaurant(Administrator administrator);
 	public void addEstablishmentNightClub(Administrator administrator);
 	public void addEstablishmentCinema(Administrator administrator);
+	
+	// READ Establishment
 	public List<Cinema> getAllCinemaEstablishment();
 	public List<NightClub> getAllNightClubEstablishment();
 	public List<Restaurant> getAllRestaurantEstablishment();
-	public void deleteAllEstablishments();
-	public void deleteEstablishmentById(int id);
+	
 	public Cinema getCinemaEstablishmentById(int id);
 	public NightClub getNightClubEstablishmentById(int id);
 	public Restaurant getRestaurantEstablishmentById(int id);
+
+	// UPDATE Establishment
+	// @timur need to add update Establishment methods
+
+	// DELETE Establishment
+	public void deleteAllEstablishments();
+	public void deleteEstablishmentById(int id);
 	public void deleteEstablishmentByType(String establishmentType);
 	
-	//User repository
+	// Person repository
+	// CREATE Person
 	public void addAdministrator(Administrator administrator);
 	public void addModerator(Administrator administrator);
 	public void addUser(Administrator administrator);
+
+	// READ Person
 	public List<Administrator> getAllAdministrators();
 	public List<Moderator> getAllModerators();
 	public List<User> getAllUsers();
-	public void deleteAllUsers();
-	public void deleteUserById(int id);
+
 	public Administrator getAdministratorById(int id);
 	public Moderator getModeratorById(int id);
 	public User getUserById(int id);
+
+	// UPDATE Person
+	// @timur need to add update Person methods
+
+	// DELETE Person
+	public void deleteAllUsers();
+	public void deleteUserById(int id);
 	
 }
